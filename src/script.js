bfb78ccff0eed6723a8a919dbb08d1c079ea5c4b
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-10-31 12:32:30 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,156 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="days col-2">
+          <div class="col-12">
+            <h4>${formatForecastDay(forecastDay.dt)}</h4>
+          </div> 
+          <div class="col-12">
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" class="small-sun-image" width=30>
+          </div>
+          <div class="col-12">
+           <span class="temp-high">${Math.round(
+             forecastDay.temp.max
+           )}°</span><span class="temp-low">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
+        </div>
+        </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "96c6ec35768d7fb6accd0167701b703c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function showTemperature(response) {
+  document.querySelector(
+    "#current-city"
+  ).innerHTML = `${response.data.name} 📍`;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity} %`;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#current-date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  celsius = response.data.main.temp;
+
+  getForecast(response.data.coord);
+}
+
+function searchWeather(event) {
+  event.preventDefault();
+  let cityDisplay = document.querySelector("#current-city");
+  let searchInput = document.querySelector("#search-text-input");
+  let city = searchInput.value;
+  cityDisplay.innerHTML = `${city} 📍`;
+  search(city);
+}
+function search(city) {
+  let apiKey = "96c6ec35768d7fb6accd0167701b703c";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiURL}&appid=${apiKey}`).then(showTemperature);
+}
+
+function getCurrentLocation(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiKey = "96c6ec35768d7fb6accd0167701b703c";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiURL}&appid=${apiKey}`).then(showTemperature);
+}
+
+function buttonClick(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentLocation);
+}
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheit);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(celsius);
+}
+
+let celsius = null;
+
+let apiKey = "96c6ec35768d7fb6accd0167701b703c";
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener("click", buttonClick);
+
+let weatherForm = document.querySelector("#weather-form");
+weatherForm.addEventListener("submit", searchWeather);
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", displayFahrenheit);
+
+let celsiusTemp = document.querySelector("#celsius");
+celsiusTemp.addEventListener("click", displayCelsius);
+
+search("Liverpool");
